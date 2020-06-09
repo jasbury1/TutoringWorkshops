d6808bfb0ca1caee59d715f0f1a3fc075720b0a5
Ryan Solorzano <rsolorza@calpoly.edu>
2020-06-08 23:21:19 -0700

    Update Part 1 in 101 String Formatting

--- 101 String Formatting/part1/stringFormatter.py
+++ 101 String Formatting/part1/stringFormatter.py
@@ -1,7 +1,14 @@
-
 def print_stats(time, speed, fuel, x_pos, y_pos):
+    # Given:
+    #  - Time as an int in seconds
+    #  - speed as a double in m/s
+    #  - fuel as a double in L
+    #  - x_pos as an int in m
+    #  - y_pos as an int in m
+    # Print to console a formatted string as shown in output.txt
     pass
 
 
 if __name__ == "__main__":
-    print_stats(100, 30.8458, 1.9541, 16, -20)
+    print_stats(1, 30.8458, 1.9541, 16, -20)
+    print_stats(100, 3.1, 0.31, 1, 100)
